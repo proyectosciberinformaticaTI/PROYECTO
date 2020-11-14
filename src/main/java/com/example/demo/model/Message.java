bc996867c8b588dbc6dc004bd302b839d67e7944
Joel Anthony Arauzo Masgo <projectinformatic1196@gmail.com>
2020-11-13 23:07:09 -0500

    Sexta version

--- src/main/java/com/example/demo/model/Message.java
+++ src/main/java/com/example/demo/model/Message.java
@@ -5,10 +5,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
-/**
- *
- * @author ard333
- */
+
 @Data @NoArgsConstructor @AllArgsConstructor @ToString
 public class Message {
 	
